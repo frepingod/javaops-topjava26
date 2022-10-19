11cd007243cae666ea7d701824e898a845fa30cc
frepingod <frepingod@gmail.com>
2022-10-19 21:15:03 +0300

    datatable via ajax

--- src/main/java/ru/javawebinar/topjava/web/RootController.java
+++ src/main/java/ru/javawebinar/topjava/web/RootController.java
@@ -8,7 +8,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import ru.javawebinar.topjava.service.MealService;
-import ru.javawebinar.topjava.service.UserService;
 import ru.javawebinar.topjava.util.MealsUtil;
 
 import javax.servlet.http.HttpServletRequest;
@@ -17,9 +16,6 @@ import javax.servlet.http.HttpServletRequest;
 public class RootController {
 
     private static final Logger log = LoggerFactory.getLogger(RootController.class);
-
-    @Autowired
-    private UserService userService;
 
     @Autowired
     private MealService mealService;
@@ -31,9 +27,8 @@ public class RootController {
     }
 
     @GetMapping("/users")
-    public String getUsers(Model model) {
+    public String getUsers() {
         log.info("users");
-        model.addAttribute("users", userService.getAll());
         return "users";
     }
 
