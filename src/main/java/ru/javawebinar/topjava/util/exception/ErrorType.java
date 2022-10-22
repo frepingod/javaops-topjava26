42d47ed62e4abde626c1918ef8481c3f8fbaf532
frepingod <frepingod@gmail.com>
2022-10-22 12:21:51 +0300

    controller advice exception

---
+++ src/main/java/ru/javawebinar/topjava/util/exception/ErrorType.java
@@ -0,0 +1,9 @@
+package ru.javawebinar.topjava.util.exception;
+
+public enum ErrorType {
+
+    APP_ERROR,
+    DATA_NOT_FOUND,
+    DATA_ERROR,
+    VALIDATION_ERROR
+}
---
+++ src/main/java/ru/javawebinar/topjava/util/exception/IllegalRequestDataException.java
@@ -0,0 +1,8 @@
+package ru.javawebinar.topjava.util.exception;
+
+public class IllegalRequestDataException extends RuntimeException {
+
+    public IllegalRequestDataException(String msg) {
+        super(msg);
+    }
+}
